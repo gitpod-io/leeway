9640fa71501b0aababddaa7a05591f856be6e9b9
Christian Weichel <christian.weichel@typefox.io>
2020-07-10 16:39:02 +0000

    [fmt] Add `leeway fmt` to format BUILD.yaml files

---
+++ cmd/fmt.go
@@ -0,0 +1,75 @@
+package cmd
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/cobra"
+	"github.com/typefox/leeway/pkg/leeway"
+)
+
+// fmtCmd represents the version command
+var fmtCmd = &cobra.Command{
+	Use:   "fmt [files...]",
+	Short: "Formats BUILD.yaml files",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		fns := args
+		if len(fns) == 0 {
+			ws, err := getWorkspace()
+			if err != nil {
+				return err
+			}
+			for _, comp := range ws.Components {
+				fns = append(fns, filepath.Join(comp.Origin, "BUILD.yaml"))
+			}
+		}
+
+		inPlace, _ := cmd.Flags().GetBool("in-place")
+		for _, fn := range fns {
+			err := formatBuildYaml(fn, inPlace)
+			if err != nil {
+				return err
+			}
+		}
+
+		return nil
+	},
+}
+
+func formatBuildYaml(fn string, inPlace bool) error {
+	f, err := os.OpenFile(fn, os.O_RDWR, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	var out io.Writer = os.Stdout
+	if inPlace {
+		buf := bytes.NewBuffer(nil)
+		out = buf
+		defer func() {
+			f.Seek(0, 0)
+			f.Truncate(0)
+
+			io.Copy(f, buf)
+		}()
+	} else {
+		fmt.Printf("---\n# %s\n", fn)
+	}
+
+	err = leeway.FormatBUILDyaml(out, f)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func init() {
+	rootCmd.AddCommand(fmtCmd)
+
+	fmtCmd.Flags().BoolP("in-place", "i", false, "format file in place rather than printing it to stdout")
+}
---
+++ pkg/leeway/format.go
@@ -0,0 +1,51 @@
+package leeway
+
+import (
+	"io"
+	"sort"
+
+	"gopkg.in/yaml.v3"
+)
+
+// FormatBUILDyaml formats a component's build.yaml file
+func FormatBUILDyaml(out io.Writer, in io.Reader) error {
+	var n yaml.Node
+	err := yaml.NewDecoder(in).Decode(&n)
+	if err != nil {
+		return err
+	}
+
+	sortPackageDeps(&n)
+
+	enc := yaml.NewEncoder(out)
+	enc.SetIndent(2)
+	return enc.Encode(&n)
+}
+
+func sortPackageDeps(n *yaml.Node) {
+	if len(n.Content) < 1 {
+		return
+	}
+
+	nde := n.Content[0]
+	for rootIdx, rootNde := range nde.Content {
+		if rootNde.Value != "packages" || rootIdx == len(nde.Content)-1 {
+			continue
+		}
+
+		nde := nde.Content[rootIdx+1]
+		if len(nde.Content) < 1 {
+			return
+		}
+		nde = nde.Content[0]
+
+		for pkgIdx, pkgNde := range nde.Content {
+			if pkgNde.Value != "deps" || pkgIdx == len(nde.Content)-1 {
+				continue
+			}
+
+			nde := nde.Content[pkgIdx+1]
+			sort.Slice(nde.Content, func(i, j int) bool { return nde.Content[j].Value < nde.Content[i].Value })
+		}
+	}
+}
