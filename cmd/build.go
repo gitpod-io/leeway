0dfd97992cb2daacce194a521c991b3fab33ead3
Christian Weichel <chris@gitpod.io>
2021-06-13 17:20:44 +0000

    [docker] Support dont-retag

--- cmd/build.go
+++ cmd/build.go
@@ -168,6 +168,7 @@ func addBuildFlags(cmd *cobra.Command) {
 	cmd.Flags().String("dump-plan", "", "Writes the build plan as JSON to a file. Use \"-\" to write the build plan to stderr.")
 	cmd.Flags().Bool("werft", false, "Produce werft CI compatible output")
 	cmd.Flags().Bool("dont-test", false, "Disable all package-level tests (defaults to false)")
+	cmd.Flags().Bool("dont-retag", false, "Disable Docker image re-tagging (defaults to false)")
 	cmd.Flags().UintP("max-concurrent-tasks", "j", uint(runtime.NumCPU()), "Limit the number of max concurrent build tasks - set to 0 to disable the limit")
 	cmd.Flags().String("coverage-output-path", "", "Output path where test coverage file will be copied after running tests")
 }
@@ -259,6 +260,11 @@ func getBuildOpts(cmd *cobra.Command) ([]leeway.BuildOption, *leeway.FilesystemC
 		log.Fatal(err)
 	}
 
+	dontRetag, err := cmd.Flags().GetBool("dont-retag")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	maxConcurrentTasks, err := cmd.Flags().GetUint("max-concurrent-tasks")
 	if err != nil {
 		log.Fatal(err)
@@ -279,6 +285,7 @@ func getBuildOpts(cmd *cobra.Command) ([]leeway.BuildOption, *leeway.FilesystemC
 		leeway.WithDontTest(dontTest),
 		leeway.WithMaxConcurrentTasks(int64(maxConcurrentTasks)),
 		leeway.WithCoverageOutputPath(coverageOutputPath),
+		leeway.WithDontRetag(dontRetag),
 	}, localCache
 }
 
