c45748a4767d2f953144920faf1eabb94953b52e
Christian Weichel <chris@gitpod.io>
2021-12-29 18:38:48 +0000

    Add warning to provenance assert --signed

--- cmd/provenance.go
+++ cmd/provenance.go
@@ -6,9 +6,10 @@ import (
 
 // provenanceCmd represents the provenance command
 var provenanceCmd = &cobra.Command{
-	Use:   "provenance <command>",
-	Short: "Helpful commands for inspecing package provenance",
-	Args:  cobra.MinimumNArgs(1),
+	Use:     "provenance <command>",
+	Short:   "Helpful commands for inspecing package provenance",
+	Args:    cobra.MinimumNArgs(1),
+	Aliases: []string{"prov"},
 }
 
 func init() {
