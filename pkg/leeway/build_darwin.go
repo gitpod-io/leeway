587c898604577f4d80f24f319ec7ddf465bf4f6c
Christian Weichel (Chris) <chris@gitpod.io>
2023-12-19 17:37:39 +0000

    Fix darwin build

---
+++ pkg/leeway/build_darwin.go
@@ -0,0 +1,9 @@
+package leeway
+
+import (
+	"fmt"
+)
+
+func executeCommandsForPackageSafe(buildctx *buildContext, p *Package, wd string, commands [][]string) error {
+	return fmt.Errorf("not implemented")
+}
