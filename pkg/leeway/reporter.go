ca4238162d35f5c67e88f818280f913400fb1fb5
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2025-03-15 21:35:03 +0000

    Fix build

--- pkg/leeway/reporter.go
+++ pkg/leeway/reporter.go
@@ -215,12 +215,7 @@ func (r *ConsoleReporter) PackageBuildFinished(pkg *Package, rep *PackageBuildRe
 	delete(r.times, nme)
 	r.mu.Unlock()
 
-	var coverage string
-	if rep.TestCoverageAvailable {
-		coverage = color.Sprintf("<fg=yellow>test coverage: %d%%</> <gray>(%d of %d functions have tests)</>\n", rep.TestCoveragePercentage, rep.FunctionsWithTest, rep.FunctionsWithTest+rep.FunctionsWithoutTest)
-	}
-	msg := color.Sprintf("%s<green>package build succeeded</> <gray>(%.2fs)</>\n", coverage, dur.Seconds())
-
+	var msg string
 	if rep.Error != nil {
 		msg = color.Sprintf("<red>package build failed while %sing</>\n<white>Reason:</> %s\n", rep.LastPhase(), rep.Error)
 	} else {
