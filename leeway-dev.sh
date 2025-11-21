60034312b1b7449d3229f457666e85b4d50629f6
Cornelius A. Ludmann <github@cornelius-ludmann.de>
2025-11-21 16:43:43 +0000

    chore: remove leeway-dev.sh helper script
    Not needed for the PR
    
    Co-authored-by: Ona <no-reply@ona.com>

--- leeway-dev.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Helper script to run leeway from source with go run
-# Usage: ./leeway-dev.sh build :package --otel-endpoint=localhost:4318
-
-cd "$(dirname "$0")"
-exec go run . "$@"
